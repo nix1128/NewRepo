55f1f190332079b855da2510d3280144cbfaeea8
Nix <sabev.nikolai@gmail.com>
2019-08-20 14:43:20 +0300

    last version

---
+++ src/containers/hoc/Auxiliary.js
@@ -0,0 +1,6 @@
+
+// HOC- technical wrapper - Hight order component, means Higher order component, used when we dont wan to use other html tags in JSX, like
+// <div> 
+const aux =(props)=> props.children;
+
+export default aux;
